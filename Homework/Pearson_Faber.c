6778ba238a58fa2e22e0b1eb3523480eb64066b8
Ryan Pearson <rpearson1994@icloud.com>
2020-02-16 17:11:16 -0500

    Working on assignment 1, Need to get parent and child code to print properly.

--- Homework/Pearson_Faber.c
+++ Homework/Pearson_Faber.c
@@ -13,6 +13,26 @@ int main()
   pid_t p;
 
   printf("Assignment 1 Ryan Pearson, James Faber.\n Enter a positive Number.\n");
+  p = fork();
+  if (p > 0)
+  {
+
+    printf("Parent process id = %d\n", getpid());
+    printf("Parent will wait until child finishes.\n");
+    wait(NULL); // The wait() system call will force parent to wait
+    printf("\n Parent resumes as child completed.\n");
+    printf("Patent done!\n\n");
+  }
+  else if (p == 0)
+  {
+    printf("Child process id = %d\n", getpid());
+    printf("\n Child executing (the ls process)..\n");
+  }
+  else
+  {
+    fprintf(stderr, "Forking failed.. code terminating");
+    return 1;
+  }
   scanf("%d", &num);
   //printf("\nYour Number is: %d", num);
   printf("pid of parent: %d\n", getpid()); //parent's id is printed
