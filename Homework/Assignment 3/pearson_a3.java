515af61578b420ba58926632ff1551a1082201fb
Ryan Pearson <rpearson1994@icloud.com>
2020-03-13 13:30:09 -0400

    Created Process class, added processes to ready queue.

--- Homework/Assignment 3/pearson_a3.java
+++ Homework/Assignment 3/pearson_a3.java
@@ -1,5 +1,33 @@
-public class Pearson_a3 {
-  public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
+public class pearson_a3 {
+
+  public class Process {
+    process(int id, int priority, int burstLength){
+      this.id = id;
+      this.priority = priority;
+      this.burstLength = burstLength;
+    }
+  }
+
+  public static void main(String[] args) {
+    // Create queue to act as ready Queue
+    Queue<Process> readyQueue = new PriorityQueue<Process>();
+    ArrayList<Integer> randomInts = new ArrayList<Integer>();
+    for (int i = 0; i < 11; i++) {
+      randomInts.add(i);
+    }
+    randomInts.shuffle();
+    for (int i = 0; i < 6; i++) {
+      int priority = ((int) Math.random * 9) + 1;
+      int burst = ((int) Math.random * 80) + 20;
+      Process p = new Process(randomInts.get(i), priority, burst);
+    }
+
+    for (int i = 0; i < readyQueue.size(); i++) {
+      System.out.print(readyQueue.remove(i));
+    }
   }
 }
