caf5b7ca2a3cbf9034135d485bbf3f08ffb65750
Ryan Pearson <rpearson1994@icloud.com>
2020-03-13 13:45:18 -0400

    Override the toString() Method to display processes properly.

--- Homework/Assignment 3/pearson_a3.java
+++ Homework/Assignment 3/pearson_a3.java
@@ -1,14 +1,24 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class pearson_a3 {
 
-  public class Process {
-    process(int id, int priority, int burstLength){
+  public static class Process {
+    private int id;
+    private int priority;
+    private int burstLength;
+
+    Process(int id, int priority, int burstLength) {
       this.id = id;
       this.priority = priority;
       this.burstLength = burstLength;
+    }
+
+    @Override
+    public String toString() {
+      return "Process: " + id + "| Priority: " + priority + "| Burst Length: " + burstLength + "\n";
     }
   }
 
@@ -19,15 +29,16 @@ public class pearson_a3 {
     for (int i = 0; i < 11; i++) {
       randomInts.add(i);
     }
-    randomInts.shuffle();
+    Collections.shuffle(randomInts);
     for (int i = 0; i < 6; i++) {
-      int priority = ((int) Math.random * 9) + 1;
-      int burst = ((int) Math.random * 80) + 20;
+      int priority = (int) (Math.random() * 9) + 1;
+      int burst = (int) (Math.random() * 80) + 20;
       Process p = new Process(randomInts.get(i), priority, burst);
+      System.out.print(p.toString());
     }
 
     for (int i = 0; i < readyQueue.size(); i++) {
-      System.out.print(readyQueue.remove(i));
+
     }
   }
 }
