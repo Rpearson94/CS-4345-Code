e5745d04e33db022f1f9b6daf2c463c3c6b05d16
Ryan Pearson <ryanpearson@Ryans-MacBook-Pro.local>
2020-04-24 23:17:52 -0400

    Added Required Comments.

--- Homework/Assignment 4/Assignment4.java
+++ Homework/Assignment 4/Assignment4.java
@@ -1,3 +1,10 @@
+/* CS4345
+ * Spring 2020
+ * Assignment 4
+ * Ryan Pearson
+ * James Faber
+*/
+
 public class Assignment4 {
   public static void main(String[] args) {
     System.out.print("Hello.");
