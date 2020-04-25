86cc862ad79ae8d320a12bea91218d7f6cf4e7fc
Ryan Pearson <rpearson1994@icloud.com>
2020-04-25 12:24:14 -0400

    Created an arraylist, added 100 cars to the array and shuffled the list.

--- Homework/Assignment 4/Assignment4.java
+++ Homework/Assignment 4/Assignment4.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 /* CS4345
  * Spring 2020
  * Assignment 4
@@ -7,6 +10,18 @@
 
 public class Assignment4 {
   public static void main(String[] args) {
-    System.out.print("Hello.");
+    // Create an arraylist of 100 cars.
+    ArrayList<Integer> cars = new ArrayList<Integer>();
+
+    for (int i = 0; i <= 100; i++) {
+      cars.add(i);
+    }
+
+    // Shuffle Cars in to random order.
+    Collections.shuffle(cars);
+
+    // for (int car : cars) {
+    // System.out.print(car);
+    // }
   }
 }
